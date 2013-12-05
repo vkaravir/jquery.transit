ab0b1611d531b40ffdd55af8fc5ec97557f84472
Ville Karavirta <vkaravir@gmail.com>
2013-12-05 18:28:38 +0200

    Fixed bug in use of properties

--- jquery.transit.js
+++ jquery.transit.js
@@ -668,7 +668,7 @@
         if (i > 0) {
           this.style[support.transition] = transitionValue;
         }
-        $(this).css(properties);
+        $(this).css(theseProperties);
       });
     };
 
